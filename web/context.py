051ad0d62401f1d7a69528acce4eeeca539f9781
J. David Ibanez <jdavid@itaapy.com>
2009-08-28 12:42:13 +0200

    web: fix transactions, fix 'context.created'

--- web/context.py
+++ web/context.py
@@ -269,21 +269,13 @@ class WebContext(HTTPContext):
 
 
     def http_get(self):
-        view = self.view
         self.commit = False
-        view.http_get(self.resource, self)
-
-        # Close transaction
-        self.close_transaction()
+        self.view.http_get(self.resource, self)
 
 
     def http_post(self):
-        view = self.view
         self.commit = True
-        view.http_post(self.resource, self)
-
-        # Close transaction
-        self.close_transaction()
+        self.view.http_post(self.resource, self)
 
 
     def close_transaction(self):
@@ -298,6 +290,7 @@ class WebContext(HTTPContext):
     # Return conditions
     #######################################################################
     def ok(self, content_type, body, wrap=True):
+        self.close_transaction()
         # Wrap
         if wrap:
             if type(body) is str:
@@ -316,7 +309,9 @@ class WebContext(HTTPContext):
 
 
     def created(self, location):
+        self.close_transaction()
         self.status = 201
+        self.method = 'GET'
         self.set_header('Location', location)
         self.del_attribute('uri')
         self.del_attribute('resource')
@@ -327,10 +322,12 @@ class WebContext(HTTPContext):
 
 
     def no_content(self):
+        self.close_transaction()
         self.status = 204
 
 
     def see_other(self, location):
+        self.close_transaction()
         if type(location) is Reference:
             location = str(location)
 
@@ -339,6 +336,7 @@ class WebContext(HTTPContext):
 
 
     def redirect(self, resource=DO_NOT_CHANGE, view=DO_NOT_CHANGE):
+        self.close_transaction()
         self.method = 'GET'
 
         if resource is not DO_NOT_CHANGE:
